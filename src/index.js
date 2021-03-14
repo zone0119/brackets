b646501a2e83c7f43b8e34c2387185e7042571cd
zone0119 <sunforsurface@gmail.com>
2021-03-15 00:52:24 +0600

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (i = 0; i < bracketsConfig.length; i++) {  
+      openbracket = bracketsConfig[i][0];
+      closedbracket = bracketsConfig[i][1];
+     }
+
+var arrBrackets = str.split('');
+// Array ["|", "(", ")", "|"]
+      for (i = 0; i < arrBrackets.length; i++) {  
+        if(i % 2 == 0)
+         arrBrackets[i]
+     }
+
 }
+
+//var str = '|()|';
+
+//var bracketsConfig = [['(', ')'], ['|', '|']]; // -> true
+//check(str, bracketsConfig);
