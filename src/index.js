8a048cbec2ca4e6e4053e64e4e95bdd125ae77df
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-01-30 17:18:43 +0600

    fix: brackets

--- src/index.js
+++ src/index.js
@@ -1,61 +1,48 @@
+
 module.exports = function check(str, bracketsConfig) {
     var openbrackets = []; 
     var closedbrackets = [];
 
   for (i = 0; i < bracketsConfig.length; i++) { 
       openbrackets.push( bracketsConfig[i][0] );
-
       closedbrackets.push( bracketsConfig[i][1] );
-     }
-console.log('openbrackets : '+ openbrackets);
-
-console.log('closedbrackets : ' + closedbrackets);
+  }
+//console.log('openbrackets : '+ openbrackets);
+//console.log('closedbrackets : ' + closedbrackets);
 //console.log(closedbrackets.includes('{'));
   
-  var ValidBrackets = false;
+var ValidBrackets = false;
 var arrBrackets = str.split('');
+  //console.log('arrBrackets : ' + arrBrackets); 
 // Array ["{", "(", ")", "}"]
    var OpenStack = [];
    var ClosedStack = [];
-      for (i = 0; i < arrBrackets.length; i++) {         
+      for (i = 0; i < arrBrackets.length; i++) {      
+       // console.log('for ##1i : ' + i + ' : bracket : ' + arrBrackets[i]); 
         
-        // open
-		if(openbrackets.includes(arrBrackets[i]))
-          OpenStack.push(arrBrackets[i]); // add open brakets
-        
-         if(closedbrackets.includes(arrBrackets[i]))
-         {
-           	if(OpenStack.length === 0) // check to open brackets
-              ValidBrackets = false;
-           	else
-            	ClosedStack.push(arrBrackets[i]); // add Closed brakets
-         }
-            
-		
-      //stack.pop();
+ closedind = closedbrackets.indexOf(arrBrackets[i]);                      
+        //   console.log(closedind + '-closedind : '); 
+         if(closedbrackets.includes(arrBrackets[i]) && OpenStack.length !==0 && openbrackets[closedind] === OpenStack[OpenStack.length-1])
+         {                         
+             let open = OpenStack.pop();
+             // console.log('tt : ' + open); 
+             ValidBrackets = true;  
+            //console.log(open + '-3f inc : ' + OpenStack); 
 
-        
+         }  
+        else		if(openbrackets.includes(arrBrackets[i]))
+        {  
+          OpenStack.push(arrBrackets[i]); // add open brakets    
+          //console.log('2t add open brakets  : ' + OpenStack); 
+        }
+        else {
+        return false;
+        }
      }
   		//collect brackets
-      console.log('OpenStack : ' + OpenStack); 
-      console.log("ClosedStack : " + ClosedStack);
-  
-  	//loop check 
-  	for (i = 0; i < OpenStack.length; i++) {  
-      
-      closedind = openbrackets.indexOf(OpenStack[i]);
-      console.log("---checkOPN : " + OpenStack[i]); 
-      
-       console.log("pos : " + ((OpenStack.length -1) - i)); 
-      console.log("sedbracke- : " + closedbrackets[closedind] ); 
-       console.log("checkCLS--- : " + ClosedStack[((OpenStack.length -1) - i)] ); 
-      if(ClosedStack[((OpenStack.length -1) - i)] == closedbrackets[closedind]) 
-       return true; 
-      else
-      return false;
-      //check in openbrackets OpenStack[i] get position closed brac
-      //check in ClosedStack closedind
-      //count-i
-  	}
-	//console.log("arrBr : " + stack.pop());
+      //console.log('END OpenStack : '); 
+   //console.log(OpenStack.length === 0); 
+    //console.log(" END ClosedStack : " + ClosedStack);
+  //console.log("ValidBrackets : " + ValidBrackets);
+  return OpenStack.length === 0;
 }
